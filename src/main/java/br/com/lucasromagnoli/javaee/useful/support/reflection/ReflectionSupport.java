c3f9938c1f6054a1cc83cbfc51da8009a37801ca
lucas.romagnoli <lucasr.romagnoli@gmail.com>
2020-08-12 00:24:04 -0300

    Criando projeto e criando classe de support a reflexão

---
+++ src/main/java/br/com/lucasromagnoli/javaee/useful/support/reflection/ReflectionSupport.java
@@ -0,0 +1,8 @@
+package br.com.lucasromagnoli.javaee.useful.support.reflection;
+
+public class ReflectionSupport {
+
+    public static void main(String[] args) {
+        System.out.println("Working in progress");
+    }
+}
